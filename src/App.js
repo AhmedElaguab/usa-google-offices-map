f1c71b1e9c53eae207ee5fceaf051f3303a0dbdf
AhmedElaguab <elaguabahmed97@gmail.com>
2019-02-25 20:33:00 +0100

    feat: add the map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import MapController from './MapController';
+import {compose, withStateHandlers} from 'recompose';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from 'react-google-maps';
+
+const MapWithAMakredInfoWindow = compose(
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={5} defaultCenter={props.defaultCenter}>
+    <Marker position={props.defaultCenter} onClick={props.onToggleOpen}>
+      {props.isOpen && <InfoWindow />}
+    </Marker>
+  </GoogleMap>
+));
 
 class App extends Component {
+  state = {
+    defaultCenter: {lat: 39.9578777, lng: -108.8299901},
+    isMarkerShown: true
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MapController />
+        <MapWithAMakredInfoWindow
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{height: `100%`}} />}
+          containerElement={<div style={{height: `800px`}} />}
+          mapElement={<div style={{height: `100%`}} />}
+          defaultCenter={this.state.defaultCenter}
+        />
       </div>
     );
   }
---
+++ src/MapController.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class MapController extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="map-controller">
+        <header>
+          <h2>Google Offices in USA</h2>
+          <form onChange={e => this.props.onStateSubmit(e.target.value)}>
+            <label htmlFor="select-state">Choose state:</label>
+            <select name="State" id="select-state">
+              <option value="All">All</option>
+              <option value="Washington">Washington</option>
+              <option value="Oregon">Oregon</option>
+              <option value="California">California</option>
+              <option value="Colorado">Colorado</option>
+            </select>
+          </form>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default MapController;
