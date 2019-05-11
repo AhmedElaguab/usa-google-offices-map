070bb1940fee578393e8174c127d141e2bec5619
AhmedElaguab <elaguabahmed97@gmail.com>
2019-05-11 18:21:25 +0200

    live preview

--- CONTRIBUTING.md
+++
@@ -1,1 +0,0 @@
-This repository is my code to pass the fifth Project of Udacity Front-End Developer Nanodegree Program (Term 2). Therefore, I most likely will not accept pull requests.
---
+++ precache-manifest.d77c898c565838248d2e389f1ae16132.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a7da37d0d91a9452ddc5",
+    "url": "/static/js/main.a7da37d0.chunk.js"
+  },
+  {
+    "revision": "1731093a1ba67260d8b3",
+    "url": "/static/js/2.1731093a.chunk.js"
+  },
+  {
+    "revision": "a7da37d0d91a9452ddc5",
+    "url": "/static/css/main.ec90dee2.chunk.css"
+  },
+  {
+    "revision": "f5547a8c1e938dfadd366fd6031c1009",
+    "url": "/index.html"
+  }
+];
